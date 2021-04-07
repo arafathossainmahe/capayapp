2699011fb08e0170b7688bda810c35a8959ec731
arafathossainmahe <54477175+arafathossainmahe@users.noreply.github.com>
2021-04-07 09:17:53 +0600

    update login page

--- lib/login.dart
+++ lib/login.dart
@@ -12,196 +12,131 @@ class _LogInState extends State<LogIn> {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: Color(0xffE2FFEE),
-      appBar: AppBar(
-        backgroundColor: Color(0xffE2FFEE),
-        elevation: 0,
-        leading: ImageIcon(
-          AssetImage('assets/icons/IconL.png'),
-        ),
-        title: Text(
-          'Forgot Your Credentials?',
-          style: TextStyle(fontSize: 12, color: Colors.black),
-        ),
-      ),
       body: Container(
         width: size.width,
         height: size.height,
         child: SingleChildScrollView(
           child: Stack(
             children: [
+              Padding(
+                padding: const EdgeInsets.only(
+                  top: 60,
+                  left: 35,
+                  right: 35,
+                ),
+                child: Container(
+                  height: 24,
+                  width: 305,
+                  child: Row(
+                    children: [
+                      Image.asset('assets/icons/IconL.png'),
+                      Text('Forgot your credential?')
+                    ],
+                  ),
+                ),
+              ),
+              //SizedBox(height: 32),
               Container(
                 child: Padding(
-                  padding: const EdgeInsets.only(top: 72, left: 148),
+                  padding: const EdgeInsets.only(top: 116, left: 148),
                   child: Image.asset('assets/icons/Thumb.png'),
                 ),
               ),
-              // Padding(
-              //   padding: EdgeInsets.only(top: 100, left: 30),
-              //   child: Text(
-              //     "Hello.",
-
-              //   ),
-              // ),
-              // Padding(
-              //   padding: EdgeInsets.only(top: 160, left: 30),
-              //   child: Text(
-              //     "Create your account",
-
-              //   ),
-              // ),
               Padding(
-                padding: EdgeInsets.only(top: 112),
+                padding: EdgeInsets.only(top: 156),
                 child: Container(
                   decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(30),
-                          topRight: Radius.circular(30))),
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(30),
+                    ),
+                  ),
                   width: double.infinity,
                   child: Padding(
                     padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
                     child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.end,
                       children: [
                         SizedBox(
                           height: 15,
                         ),
                         Center(
                           child: Text(
-                            "Sign Up",
+                            "Let's Sign up",
+                            style: TextStyle(
+                                fontSize: 24, fontWeight: FontWeight.bold),
                           ),
                         ),
                         SizedBox(
                           height: 10,
                         ),
-                        TextFormField(
-                          decoration: InputDecoration(
-                            filled: true,
-                            fillColor: Color(0xffedffee),
-                            border: UnderlineInputBorder(
-                                borderSide: BorderSide.none),
-                            labelText: "Email",
-                          ),
-                        ),
+                        Text('Wellcome back you have been missed'),
                         SizedBox(
-                          height: 10,
+                          height: 56,
                         ),
+                        Text('Phone Number'),
                         TextFormField(
                           decoration: InputDecoration(
                               filled: true,
                               fillColor: Color(0xffedffee),
                               border: UnderlineInputBorder(
                                   borderSide: BorderSide.none),
-                              labelText: "Password",
+                              hintText: '+880'),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text('Password'),
+                        TextFormField(
+                          decoration: InputDecoration(
+                              filled: true,
+                              fillColor: Color(0xffedffee),
+                              border: UnderlineInputBorder(
+                                  borderSide: BorderSide.none),
+                              hintText: "Password",
                               suffixIcon: Icon(
                                 Icons.remove_red_eye,
                                 color: Color(0xff29d65a).withOpacity(0.4),
                               )),
                         ),
                         SizedBox(
-                          height: 10,
+                          height: 134,
                         ),
-                        TextFormField(
-                          decoration: InputDecoration(
-                            filled: true,
-                            fillColor: Color(0xffedffee),
-                            border: UnderlineInputBorder(
-                                borderSide: BorderSide.none),
-                            labelText: "Confirm Password",
-                          ),
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        Center(
-                          child: FlatButton(
-                            onPressed: null,
-                            child: Material(
-                              elevation: 10,
-                              borderRadius: BorderRadius.circular(10),
-                              child: Container(
-                                width: 180,
-                                height: 50,
-                                decoration: BoxDecoration(
-                                    gradient: LinearGradient(
-                                      colors: [
-                                        Color(0xff29d65a),
-                                        Color(0xff54d94a)
-                                      ],
-                                      begin: Alignment.topLeft,
-                                      end: Alignment.bottomRight,
-                                    ),
-                                    borderRadius: BorderRadius.circular(10)),
-                                child: Center(
-                                  child: Text(
-                                    "Sign Up",
-                                  ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: <Widget>[
+                            FlatButton(
+                              color: Colors.green,
+                              child: Text(
+                                'Sign In',
+                                style: TextStyle(
+                                  fontSize: 14,
                                 ),
                               ),
-                            ),
-                          ),
-                        ),
-                        SizedBox(
-                          height: 30,
-                        ),
-                        Row(
-                          children: [
-                            Expanded(
-                              child: Divider(
-                                height: 20,
-                                thickness: 2,
-                                indent: 30,
-                                color: Colors.black38,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
                               ),
+                              padding: const EdgeInsets.all(20),
+                              textColor: Colors.white,
+                              onPressed: () {},
                             ),
-                            Text(
-                              " Or sign in with ",
-                            ),
-                            Expanded(
-                              child: Divider(
-                                height: 20,
-                                thickness: 2,
-                                endIndent: 30,
-                                color: Colors.black38,
+                            FlatButton(
+                              color: Colors.grey[100],
+                              child: Text(
+                                'Create an account',
+                                style: TextStyle(
+                                  fontSize: 14,
+                                ),
                               ),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              padding: const EdgeInsets.all(20),
+                              textColor: Colors.black,
+                              onPressed: () {},
                             ),
                           ],
                         ),
-                        SizedBox(
-                          height: 25,
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Material(
-                              elevation: 10,
-                              borderRadius: BorderRadius.circular(10),
-                              child: Padding(
-                                padding: const EdgeInsets.all(4.0),
-                                child: Image.network(
-                                  'https://i.postimg.cc/6qMTfNpM/google-symbol.png',
-                                  height: 25,
-                                  width: 25,
-                                ),
-                              ),
-                            ),
-                            SizedBox(
-                              width: 20,
-                            ),
-                            Material(
-                              elevation: 10,
-                              borderRadius: BorderRadius.circular(10),
-                              child: Padding(
-                                padding: const EdgeInsets.all(4.0),
-                                child: Image.network(
-                                  'https://i.postimg.cc/wT9GtYNS/twitter-4.png',
-                                  height: 25,
-                                  width: 25,
-                                ),
-                              ),
-                            ),
-                          ],
-                        )
                       ],
                     ),
                   ),
