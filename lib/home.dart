c9058e4aa6327068a546b84ce81027b738625f59
arafathossainmahe <54477175+arafathossainmahe@users.noreply.github.com>
2021-04-27 23:43:52 +0600

    home

--- lib/home.dart
+++ lib/home.dart
@@ -26,16 +26,74 @@ class _HomeState extends State<Home> {
               child: Column(
                 children: [
                   ListTile(
-                    
                     leading: Icon(Icons.search),
                     title: Text('Search on CaPay'),
                   ),
+                  SizedBox(
+                    height: 48,
+                  ),
+                  Title(
+                    color: Colors.black,
+                    child: Text(
+                      'TK 1600.00',
+                      style: TextStyle(fontSize: 24),
+                    ),
+                  ),
+                  Text('CaPay Balance'),
+                  Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(top: 32, left: 10),
+                        child: cContainer(),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 32, left: 10),
+                        child: cContainer(),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 32, left: 10),
+                        child: cContainer(),
+                      )
+                    ],
+                  ),
                 ],
               ),
+            ),
+          ),
+          SizedBox(height: 40,),
+          Container(
+            height: 1383,
+            width: 325,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(25),
             ),
           ),
         ],
       ),
     );
   }
+}
+
+Widget cContainer() {
+  return Container(
+    height: 68,
+    width: 95,
+    decoration: BoxDecoration(
+      color: Colors.white,
+      borderRadius: BorderRadius.circular(15),
+    ),
+    child: Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(top: 12),
+          child: Image.asset('assets/icons/Icon1.png'),
+        ),
+        SizedBox(
+          height: 5,
+        ),
+        Text('Top up')
+      ],
+    ),
+  );
 }
